508ebcbd83e038af1b79f0544635caf61e83380e
Vincent <vincent.dupont12@gmail.com>
2026-04-19 20:51:04 +0200

    creator V5.11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 
 export default function BrewMasterPotes() {
   const [view, setView] = useState("home");
