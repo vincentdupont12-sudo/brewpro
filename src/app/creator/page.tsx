b0fbea7f8e2fe503cbe9848460d28958d8fb1e95
Vincent <vincent.dupont12@gmail.com>
2026-04-19 21:35:37 +0200

    creator V5.16

--- src/app/creator/page.tsx
+++ src/app/creator/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect, useCallback } from "react";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../../../lib/supabaseClient";
 
 export default function SuperLaboUltime() {
   const [dbInventory, setDbInventory] = useState<any[]>([]);
